fc854a1f60e1e765996185d9063609862cc7d461
latthisit <152863025+latthisit@users.noreply.github.com>
2025-12-16 00:05:10 +0700

    Update account.js

--- server/routes/account.js
+++ server/routes/account.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { login, logout } = require('../controllers/accountController');
 const { validate } = require('../middleware/account');
+const auth = require('../middleware/auth');
 
 router.post('/login', validate, login);
 router.post('/logout', auth, logout);
